90f5a6e649a4fe1632337f42d45a643b1d2f0f1a
Babar-Hans <babarhans34@gmail.com>
2024-02-17 15:14:28 +0500

    project done

--- bin/credential.dart
+++ bin/credential.dart
@@ -37,5 +37,37 @@ userLoginCredential() {
 
 actionTodo() {
   print("Enter Your Action! ");
+  print("[1.add = To add your todo]");
+  print("[2.remove = To add your todo]");
+  print("[1.show = To add your todo]");
+
   String choice = stdin.readLineSync()!;
+  print("");
+
+  while (choice != "exit") {
+    if (choice == "add") {
+      do {
+        stdout.write("Enter the Todo you want to add: ");
+        String item = stdin.readLineSync()!;
+        print("");
+        myTodo.addItem(item);
+        print("Do you want to add more items (y/n)");
+        String ynChar = stdin.readLineSync()!;
+        if (ynChar != "y") {
+          break; // Exit the loop if the user does not want to add more items
+        }
+      } while (true);
+    } else if (choice == "remove") {
+      stdout.write("Enter the Todo you want to remove: ");
+      String item = stdin.readLineSync()!;
+      myTodo.removeItem(item);
+    } else if (choice == "show") {
+      myTodo.showItem(myTodo.list);
+    }
+
+    // Prompt for next action
+    print("\nEnter Your Next Action! ");
+    choice = stdin.readLineSync()!;
+    print("");
+  }
 }
--- bin/todo.dart
+++ bin/todo.dart
@@ -1,12 +1,20 @@
 class Todo {
-  List<dynamic> list = [];
+  List<String> list = [];
 
   //method1 Add Todo Item
-  void addItem(String item) {}
+  void addItem(String item) {
+    list.add(item);
+  }
 
   //method2 Remove Todo Item
-  void removeItem() {}
+  void removeItem(String item) {
+    list.remove(item);
+  }
 
   //method3 showAll Todo Item
-  void showItem() {}
+  void showItem(List<String> list) {
+    for (String newList in list) {
+      print(newList);
+    }
+  }
 }
